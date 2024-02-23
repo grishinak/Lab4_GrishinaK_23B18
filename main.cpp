ead0aee1ffd18bf25678291b077ebafaf9f318ef
grishinak <st122261@student.spbu.ru>
2024-02-23 12:27:47 +0300

    cout corretions

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     int E = reader.getE();//сохранение числа ребер
     std::vector<std::pair<int, int>> edges = reader.getEdges();//сохранение пар вершин, связанных ребром
 
-    std::cout << "Number of vertices11 вершин: " << V << std::endl;//вывод числа вершин
+    std::cout << "Number of vertices: " << V << std::endl;//вывод числа вершин
     std::cout << "Number of edges: " << E << std::endl;//вывод числа ребер
 
     for (const auto& edge : edges) {
