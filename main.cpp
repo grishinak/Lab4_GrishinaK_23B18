5288746f31c0f4753c3b8dc3076dc9cb6502e3f6
grishinak <st122261@student.spbu.ru>
2024-03-02 05:25:34 +0300

    added r g b atributes to drawVertices and drawVertexNumbers

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include "masks.h"
 #include "Graph.h"
 
-void drawVertices(Graph& graph, Painter& painter);
+void drawVertices(Graph& graph, Painter& painter,std::uint8_t r, std::uint8_t g, std::uint8_t b);
 void drawEdges(Graph& graph, Painter& painter, std::uint8_t r, std::uint8_t g, std::uint8_t b);
-void drawVertexNumbers(Painter& painter,  Graph& graph,  std::vector<std::vector<std::vector<int>>>& masks) ;
+void drawVertexNumbers(Painter& painter,  Graph& graph,  std::vector<std::vector<std::vector<int>>>& masks,
+std::uint8_t r, std::uint8_t g, std::uint8_t b) ;
 
 int main() {
 
@@ -39,10 +40,10 @@ int main() {
     drawEdges(graph, painter, 0, 0, 255);
 
     //draw vertices
-    drawVertices(graph, painter);
+    drawVertices(graph, painter, 255,0,0);
 
     //draw vertex numbers
-    drawVertexNumbers(painter, graph, masks);
+    drawVertexNumbers(painter, graph, masks, 0, 0, 0);
 
 
         //saving output file     
@@ -52,10 +53,10 @@ int main() {
     return 0;
 }
 
-void drawVertices(Graph& graph, Painter& painter){
+void drawVertices(Graph& graph, Painter& painter,std::uint8_t r, std::uint8_t g, std::uint8_t b){
 for (int i = 0; i < graph.getV(); ++i) {
     Point point = graph.getPosition(i);
-    painter.drawCircle(point.x, point.y, 255, 0, 0);
+    painter.drawCircle(point.x, point.y, r, g, b);
     }
 }
 
@@ -67,35 +68,36 @@ void drawEdges(Graph& graph, Painter& painter, std::uint8_t r, std::uint8_t g, s
     }
 }
 
-void drawVertexNumbers(Painter& painter,  Graph& graph,  std::vector<std::vector<std::vector<int>>>& masks) {
+void drawVertexNumbers(Painter& painter,  Graph& graph,  std::vector<std::vector<std::vector<int>>>& masks,
+std::uint8_t r, std::uint8_t g, std::uint8_t b) {
     // Проходим по каждой вершине графа
     for (int i = 0; i < graph.getV(); ++i) { 
         Point point = graph.getPosition(i); // Получаем позицию вершины
         
         // Рассматриваем различные случаи для отображения номера вершины
         if (i < 10) {
-            painter.drawDigit(masks[i % 10], point.x - 5, point.y - 17, 0, 0, 0);
+            painter.drawDigit(masks[i % 10], point.x - 5, point.y - 17, r, g, b);
         } else if (i < 100) {
             int units = i % 10;
             int tens = i / 10;
-            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, 0, 0, 0);
+            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, r, g, b);
+            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, r, g, b);
         } else if (i < 1000) {
             int units = i % 10;
             int tens = (i / 10) % 10;
             int hundreds = i / 100;
-            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[hundreds % 10], point.x - 25, point.y - 17, 0, 0, 0);
+            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, r, g, b);
+            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, r, g, b);
+            painter.drawDigit(masks[hundreds % 10], point.x - 25, point.y - 17, r, g, b);
         } else if (i < 10000) {
             int units = i % 10;
             int tens = (i / 10) % 10;
             int hundreds = (i / 100) % 10;
             int thousands = i / 1000;
-            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[hundreds % 10], point.x - 25, point.y - 17, 0, 0, 0);
-            painter.drawDigit(masks[thousands % 10], point.x - 35, point.y - 17, 0, 0, 0);
+            painter.drawDigit(masks[units % 10], point.x - 5, point.y - 17, r, g, b);
+            painter.drawDigit(masks[tens % 10], point.x - 15, point.y - 17, r, g, b);
+            painter.drawDigit(masks[hundreds % 10], point.x - 25, point.y - 17, r, g, b);
+            painter.drawDigit(masks[thousands % 10], point.x - 35, point.y - 17, r, g, b);
         }
     }
 }
