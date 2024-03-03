5a95b01e6d451f67d9293eb7b785fef9a679e6e7
grishinak <st122261@student.spbu.ru>
2024-03-03 18:57:11 +0300

    made a function to print edges/ removed it to function.h

--- main.cpp
+++ main.cpp
@@ -6,46 +6,38 @@
 #include "masks.h"
 #include "functions.h"
 
+void PrintEdges(std::vector<std::pair<int, int>> _edges ) {
+     for (const auto& edge : _edges) {
+        std::cout << edge.first << " " << edge.second << std::endl;
+    }
+}
+
+
 int main() {
 
-    /* //inputreader test */
-    InputReader reader;//создание обьекта класса 
+    InputReader reader;
     int V = reader.GetV();//сохранение числа вершин
     int E = reader.GetE();//сохранение числа ребер
     std::vector<std::pair<int, int>> edges = reader.GetEdges();//сохранение пар вершин, связанных ребром
 
     std::cout << "Number of vertices: " << V << std::endl;//вывод числа вершин
     std::cout << "Number of edges: " << E << std::endl;//вывод числа ребер
+    PrintEdges(edges);//вывод пар вершин, связанных ребром
 
-    for (const auto& edge : edges) {
-        std::cout << edge.first << " " << edge.second << std::endl;
-    }//вывод пар вершин, связанных ребром
-// all printed out from inputreader
 
-    /*graph test*/
     Graph graph(V);
     graph.AddEdges(edges);//добавили считанные ребра из ввода
     graph.Planarize();//планаризовали по силам
     graph.PrintPositions();// вывели координаты вершин 
-//all printed out from graph
 
-    /* painter test*/
+
     Painter painter(2000, 2000);
- 
-        //draw edges
     DrawEdges(graph, painter, 0, 0, 255);
-
-    //draw vertices
     DrawVertices(graph, painter, 255,0,0);
-
-    //draw vertex numbers
     DrawVertexNumbers(painter, graph, masks, 0, 0, 0);
 
-
-        //saving output file     
     painter.Save("output.bmp");
     std::cout << "Image saved successfully!\n";
 
     return 0;
 }
-
